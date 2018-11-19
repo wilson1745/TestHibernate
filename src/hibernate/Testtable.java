05709f00aed05d35abb4cdbbbdc58dc2a2c22bda
WilsonLo <wilsonlo@cybersoft4u.com>
2018-11-19 17:56:17 +0800

    Hibernate init

---
+++ src/hibernate/Testtable.java
@@ -0,0 +1,41 @@
+package hibernate;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "testtable")
+public class Testtable {
+
+	private int id;
+	private String username;
+	private String number;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Column(name = "username")
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Column(name = "number")
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+}
